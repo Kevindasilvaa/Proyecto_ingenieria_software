f1abac4ed2a189636bae56af27a9e2a932c3181d
gabo2105MS <122235345+gabo2105MS@users.noreply.github.com>
2025-02-14 13:03:21 -0400

    Category fix

--- lib/models/clases/category.dart
+++ lib/models/clases/category.dart
@@ -1,3 +1,42 @@
+// import 'package:flutter/material.dart';
+
+// class Category {
+//   final String id;
+//   final String name;
+//   final String icon;
+//   final Color color;
+//   final String user_email;
+
+//   Category({
+//     required this.id,
+//     required this.name,
+//     required this.icon,
+//     required this.color,
+//     required this.user_email,
+//   });
+
+//   // Convertir un objeto a JSON para guardarlo en Firebase
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'id': id,
+//       'name': name,
+//       'icon': icon,
+//       'color': color.value, // Guardamos el color como un número entero
+//       'user_email':user_email,
+//     };
+//   }
+
+//   // Convertir un documento de Firebase en una categoría
+//   factory Category.fromMap(Map<String, dynamic> map, String id) {
+//     return Category(
+//       id: id,
+//       name: map['name'] ?? '',
+//       icon: map['icon'] ?? '📂', // Si no hay icono, se asigna uno por defecto
+//       color: Color(map['color'] ?? 0xFF000000), // Si no hay color, usa negro
+//       user_email: map['user_email'],
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class Category {
@@ -5,12 +44,14 @@ class Category {
   final String name;
   final String icon;
   final Color color;
+  final String user_email;
 
   Category({
     required this.id,
     required this.name,
     required this.icon,
     required this.color,
+    required this.user_email,
   });
 
   // Convertir un objeto a JSON para guardarlo en Firebase
@@ -20,16 +61,27 @@ class Category {
       'name': name,
       'icon': icon,
       'color': color.value, // Guardamos el color como un número entero
+      'user_email': user_email,
     };
   }
 
   // Convertir un documento de Firebase en una categoría
   factory Category.fromMap(Map<String, dynamic> map, String id) {
+    // Verificar si el email es nulo y asignar un valor predeterminado
+    String? email = map['user_email'];
+
+    // Si user_email es nulo, se asigna un valor genérico para todos los usuarios
+    if (email == null || email.isEmpty) {
+      email = 'all_users@domain.com'; // Valor para indicar acceso a todos los usuarios
+    }
+
     return Category(
       id: id,
-      name: map['name'] ?? '',
-      icon: map['icon'] ?? '📂', // Si no hay icono, se asigna uno por defecto
-      color: Color(map['color'] ?? 0xFF000000), // Si no hay color, usa negro
+      name: map['name'] ?? '', // Nombre vacío si no está presente
+      icon: map['icon'] ?? '📂', // Icono por defecto si no está presente
+      color: Color(map['color'] ?? 0xFF000000), // Color por defecto si no está presente
+      user_email: email, // Email asignado
     );
   }
 }
+
