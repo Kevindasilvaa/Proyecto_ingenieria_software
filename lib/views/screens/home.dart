5e1f1f708cf188f2d53639d95ae36cfed68f407f
Kevin <kevin@MacBook-Air-de-Kevin.local>
2025-02-17 00:16:29 -0400

    avances home

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -1,13 +1,162 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:moni/controllers/cuenta_controller.dart';
+// import 'package:moni/controllers/user_controller.dart';
+// import 'package:provider/provider.dart';
+// import 'package:moni/views/widgets/NavBar.dart';
+// import 'package:moni/views/widgets/CustomDrawer.dart';
+// import 'package:moni/views/widgets/RecentTransactionsView.dart';
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   bool _isLoading = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       FirebaseAuth.instance.authStateChanges().listen((User? user) {
+//         if (user == null) {
+//           Navigator.of(context).pushNamed('/');
+//         } else {
+//           _loadData(user);
+//         }
+//       });
+//     });
+//   }
+
+//   Future<void> _loadData(User user) async {
+//     final cuentaController = Provider.of<CuentaController>(context, listen: false);
+
+//     try {
+//       await cuentaController.cargarCuentas(user.email!);
+//       setState(() {
+//         _isLoading = false;
+//       });
+//     } catch (error) {
+//       print('Error al cargar datos: $error');
+//       setState(() {
+//         _isLoading = false;
+//       });
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text('Error al cargar las cuentas: $error')),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final userController = Provider.of<UserController>(context);
+//     final cuentaController = Provider.of<CuentaController>(context); // Access cuentaController here
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: const Color(0xFFFFFFFF),
+//         title: const Text('Home', style: TextStyle(color: Colors.black)),
+//         iconTheme: const IconThemeData(color: Colors.black),
+//       ),
+//       drawer: CustomDrawer(),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: _isLoading
+//                 ? const Center(child: CircularProgressIndicator())
+//                 : SafeArea(
+//               child: Padding(
+//                 padding: const EdgeInsets.all(16.0),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     _buildBalanceCard(cuentaController), // Pass cuentaController
+//                     const SizedBox(height: 20),
+//                     const Text('Recent Transactions',
+//                         style: TextStyle(
+//                             fontSize: 18, fontWeight: FontWeight.bold)),
+//                     const SizedBox(height: 10),
+//                     Expanded(
+//                       child: RecentTransactionsView(),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//           Container(
+//             height: 80.0,
+//             child: NavBar(
+//               onPlusPressed: () {
+//                 Navigator.of(context).pushNamed('/addTransactions');
+//               },
+//               currentPage: '/home',
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildBalanceCard(CuentaController cuentaController) { // Receive cuentaController
+//     return Container(
+//       padding: const EdgeInsets.all(20),
+//       decoration: BoxDecoration(
+//         color: const Color(0xFFF2F2F2),
+//         borderRadius: BorderRadius.circular(10),
+//       ),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           const Text('Total Balance', style: TextStyle(fontSize: 16)),
+//           Text(
+//             '\$${cuentaController.calcularBalanceTotal()}',
+//             style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//           ),
+//           const SizedBox(height: 10),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: [
+//               Column(
+//                 children: [
+//                   const Text('Total Income',
+//                       style: TextStyle(color: Colors.green)),
+//                   Text('\$${'Falta por hacer'}',
+//                       style: const TextStyle(color: Colors.green)),
+//                 ],
+//               ),
+//               Column(
+//                 children: [
+//                   const Text('Total Expense',
+//                       style: TextStyle(color: Colors.red)),
+//                   Text('-\$${'Falta por hacer'}',
+//                       style: const TextStyle(color: Colors.red)),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Future<void> _logout() async {
+//     final userController = Provider.of<UserController>(context, listen: false);
+//     await userController.logOut();
+//     ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Cierre de sesión exitoso.')));
+//     Navigator.pushNamed(context, '/');
+//   }
+// }
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:moni/controllers/cuenta_controller.dart';
 import 'package:moni/controllers/user_controller.dart';
-import 'package:moni/models/clases/Usuario.dart';
 import 'package:provider/provider.dart';
 import 'package:moni/views/widgets/NavBar.dart';
 import 'package:moni/views/widgets/CustomDrawer.dart';
-import 'package:moni/controllers/transaccion_controller.dart';
-import 'package:moni/models/clases/transaccion.dart'; 
-import 'package:intl/intl.dart';
+import 'package:moni/views/widgets/RecentTransactionsView.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,46 +164,127 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isLoading = true;
+
   @override
   void initState() {
     super.initState();
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user == null) {
-        print('User is currently signed out!');
-        Navigator.of(context).pushNamed('/');
-      } else {
-        print('User is signed in!');
-      }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null) {
+          Navigator.of(context).pushNamed('/');
+        } else {
+          _loadData(user);
+        }
+      });
     });
+  }
+
+  Future<void> _loadData(User user) async {
+    final cuentaController = Provider.of<CuentaController>(context, listen: false);
+
+    try {
+      await cuentaController.cargarCuentas(user.email!);
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (error) {
+      print('Error al cargar datos: $error');
+      setState(() {
+        _isLoading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al cargar las cuentas: $error')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final userController = Provider.of<UserController>(context);
+    final cuentaController = Provider.of<CuentaController>(context);
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xFFCECECE),
-        title: Text('Home'),
+        backgroundColor: const Color(0xFFFFFFFF),
+        title: const Text('Home', style: TextStyle(color: Colors.black)),
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
       drawer: CustomDrawer(),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: TransaccionesView(), // Usa la clase TransaccionesView
-            ),
-            Container(
-              height: 80.0,
-              child: NavBar(
-                onPlusPressed: () {
-                  Navigator.of(context).pushNamed('/addTransactions');
-                },
-                currentPage: '/home',
+      body: Column( // <-- Main Column
+        children: [
+          Expanded( // <-- Expanded for the main content
+            child: _isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : Padding( // <-- Padding instead of SafeArea
+              padding: const EdgeInsets.all(16.0),
+              child: Column( // <-- Column inside Padding
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildBalanceCard(cuentaController),
+                  const SizedBox(height: 20),
+                  const Text('Recent Transactions',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: RecentTransactionsView(),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Container( // <-- NavBar remains outside Expanded
+            height: 80.0,
+            child: NavBar(
+              onPlusPressed: () {
+                Navigator.of(context).pushNamed('/addTransactions');
+              },
+              currentPage: '/home',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBalanceCard(CuentaController cuentaController) { // Receive cuentaController
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: const Color(0xFFF2F2F2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Total Balance', style: TextStyle(fontSize: 16)),
+          Text(
+            '\$${cuentaController.calcularBalanceTotal()}',
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const Text('Total Income',
+                      style: TextStyle(color: Colors.green)),
+                  Text('\$${'Falta por hacer'}',
+                      style: const TextStyle(color: Colors.green)),
+                ],
+              ),
+              Column(
+                children: [
+                  const Text('Total Expense',
+                      style: TextStyle(color: Colors.red)),
+                  Text('-\$${'Falta por hacer'}',
+                      style: const TextStyle(color: Colors.red)),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -63,55 +293,7 @@ class _HomePageState extends State<HomePage> {
     final userController = Provider.of<UserController>(context, listen: false);
     await userController.logOut();
     ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Cierre de sesión exitoso.')));
+        const SnackBar(content: Text('Cierre de sesión exitoso.')));
     Navigator.pushNamed(context, '/');
-  }
-}
-
-// Muestra las 3 ultimas transacciones segun su fecha
-class TransaccionesView extends StatelessWidget { // Cambiado a StatelessWidget
-  final TransaccionesController _controller = TransaccionesController();
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<Transaccion>>( // Usamos StreamBuilder
-      stream: _controller.obtenerTransaccionesUsuarioStream(), // Escuchamos el stream
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator()); // Mientras carga
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}')); // Si hay error
-        } else if (snapshot.data == null || snapshot.data!.isEmpty) {
-          return Center(child: Text('No hay transacciones')); // Si no hay datos
-        } else {
-          final transacciones = snapshot.data!;
-
-          // Ordena las transacciones por fecha de forma descendente (las más recientes primero)
-          transacciones.sort((a, b) => b.fecha.compareTo(a.fecha));
-
-          // Toma solo las últimas 3 transacciones
-          final ultimasTransacciones = transacciones.take(3).toList();
-
-          return ListView.builder(
-            itemCount: ultimasTransacciones.length,
-            itemBuilder: (context, index) {
-              final transaccion = ultimasTransacciones[index];
-              return ListTile(
-                title: Text(transaccion.nombre),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Categoría: ${transaccion.categoria}'),
-                    Text('Monto: ${transaccion.monto}'),
-                    Text('Ingreso: ${transaccion.ingreso}'),
-                    Text('Fecha: ${DateFormat.yMd().format(transaccion.fecha)}'),
-                  ],
-                ),
-              );
-            },
-          );
-        }
-      },
-    );
   }
 }
---
+++ lib/views/widgets/RecentTransactionsView.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:moni/controllers/transaccion_controller.dart';
+import 'package:moni/models/clases/transaccion.dart';
+import 'package:intl/intl.dart';
+
+//Este widget retorna las ultimas tres transacciones del usuario
+class RecentTransactionsView extends StatelessWidget {
+  final TransaccionesController _controller = TransaccionesController();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<Transaccion>>(
+      stream: _controller.obtenerTransaccionesUsuarioStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.data == null || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No hay transacciones'));
+        } else {
+          final transacciones = snapshot.data!;
+
+          // Ordena las transacciones por fecha de forma descendente (las más recientes primero)
+          transacciones.sort((a, b) => b.fecha.compareTo(a.fecha));
+
+          // Toma solo las últimas 3 transacciones
+          final ultimasTransacciones = transacciones.take(3).toList();
+
+          return ListView.builder(
+            itemCount: ultimasTransacciones.length,
+            itemBuilder: (context, index) {
+              final transaccion = ultimasTransacciones[index];
+              return Card( // Envuelve cada transacción en una Card
+                margin: const EdgeInsets.symmetric(vertical: 8.0), // Márgenes para separar las tarjetas
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on, color: transaccion.ingreso ? Colors.green : Colors.red), // Icono de ingreso/gasto
+                  title: Text(transaccion.nombre),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Categoría: ${transaccion.categoria}'),
+                      Text('Monto: ${transaccion.monto}'),
+                      Text('Fecha: ${DateFormat.yMd().format(transaccion.fecha)}'),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+}
