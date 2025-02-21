7ed5b1d61ac380c25f594340e0fc8ae6dc6b3cbc
Kevin <kevin@MacBook-Air-de-Kevin.local>
2025-02-21 19:18:18 -0400

    mejora diseno login y register

--- lib/views/screens/register.dart
+++ lib/views/screens/register.dart
@@ -1,63 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:moni/controllers/user_controller.dart'; // Importa el controlador
-import 'package:moni/models/clases/Usuario.dart'; // Importa la clase User
+import 'package:moni/controllers/user_controller.dart';
+import 'package:moni/models/clases/Usuario.dart';
 import 'package:moni/views/widgets/CustomButton.dart';
 import 'package:moni/views/widgets/CustomTextField.dart';
 import 'package:moni/views/widgets/GoogleSignInButton.dart';
 import 'package:provider/provider.dart';
 
-class RegisterPage extends StatefulWidget { // Renombra la clase a RegisterPage
+class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
-class _RegisterPageState extends State<RegisterPage> { // Renombra el estado
+class _RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  final UserController _userController = UserController(); // Instancia del controlador
-  bool _isPasswordVisible = false; // funciona para saber el estado del icono del input password
+  final UserController _userController = UserController();
+  bool _isPasswordVisible = false;
 
   Future<void> createAccount() async {
-  if (_formKey.currentState!.validate()) {
-    try {
-      Usuario? newUser = await _userController.createUser(_emailController.text, _passwordController.text);
+    if (_formKey.currentState!.validate()) {
+      try {
+        Usuario? newUser = await _userController.createUser(
+            _emailController.text, _passwordController.text);
 
-      if(newUser != null){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cuenta creada exitosamente.")),);
-        Navigator.pushReplacementNamed(context, '/home');
-      }else{
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error al crear la cuenta.")),);
+        if (newUser != null) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Cuenta creada exitosamente.")),
+          );
+          Navigator.pushReplacementNamed(context, '/home');
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Error al crear la cuenta.")),
+          );
+        }
+      } catch (e) {
+        print("Error general al crear usuario: $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error general al crear usuario: $e")),
+        );
       }
-    } catch (e) {
-      print("Error general al crear usuario: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error general al crear usuario: $e")),
-      );
     }
   }
-}
 
-Future<void> createAccountWithGoogle() async {
+  Future<void> createAccountWithGoogle() async {
     try {
       final userController = Provider.of<UserController>(context, listen: false);
       await userController.signInWithGoogle();
 
       if (userController.usuario != null) {
-        // Navegar a la siguiente pantalla
         Navigator.pushReplacementNamed(context, '/home');
         ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Se inicio sesion exitosamente')),
-      );
+          SnackBar(content: Text('Se inicio sesion exitosamente')),
+        );
       } else {
-        // Mostrar mensaje de error
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text("Ocurrio un error.")),
         );
       }
     } catch (e) {
-      print('Error en signInWithGoogle en LoginPage: $e');
-      // Mostrar un mensaje de error genérico en la UI
+      print('Error en signInWithGoogle en RegisterPage: $e');
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error al crear cuenta con Google')),
       );
@@ -67,67 +69,160 @@ Future<void> createAccountWithGoogle() async {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Crear Cuenta'), // Cambia el título
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              // Input Email
-              CustomTextField(
-                controller: _emailController,
-                labelText: 'Email',
-                hintText: 'Ingresa tu email',
-                prefixIcon: Icons.email,
-                suffixIcon: Icons.close,
-                keyboardType: TextInputType.emailAddress,
-                onSuffixIconTap: () {
-                  _emailController.clear(); // Limpia el contenido del email
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Por favor, ingresa tu email';
-                  }
-                  return null;
-                },
+      body: Stack(
+        children: [
+          // Fondo gris superior con título y flecha
+          Container(
+            height: MediaQuery.of(context).size.height * 0.30,
+            decoration: BoxDecoration(
+              color: const Color(0xFFF2F2F2),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
               ),
-              // Input Password
-              CustomTextField(
-                controller: _passwordController,
-                labelText: 'Contraseña',
-                hintText: 'Ingresa tu contraseña',
-                prefixIcon: Icons.lock, // Ícono de candado para la contraseña
-                suffixIcon: _isPasswordVisible ? Icons.visibility : Icons.visibility_off, // Alterna entre visibilidad y ocultación
-                keyboardType: TextInputType.text,
-                obscureText: !_isPasswordVisible, // Alterna la visibilidad de la contraseña
-                onSuffixIconTap: () {
-                  setState(() {
-                    _isPasswordVisible = !_isPasswordVisible; // Cambia el estado de visibilidad
-                  });
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Por favor, ingresa tu contraseña';
-                  }
-                  return null;
-                },
-              ),
-              CustomButton(
-                onPressed: createAccount,
-                text: 'CREAR CUENTA',
-              ),
-              SizedBox(height: 20), // 20 píxeles de espacio vertical
-              GoogleSignInButton(
-                onPressed: () {
-                  createAccountWithGoogle();
-                },
-              ),
-            ],
+            ),
+            child: Stack(
+              children: [
+                // Flecha en la parte superior izquierda
+                Positioned(
+                  top: 30,
+                  left: 16,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                  ),
+                ),
+                // Título centrado
+                Center(
+                  child: Text(
+                    'Crear Cuenta',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.15,
+              decoration: BoxDecoration(
+                color: const Color(0xFFF2F2F2),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: const Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Al crear una cuenta, aceptas los Términos de Servicio y la Política de Privacidad de MONI.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Contenido principal centrado
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(24.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(height: 60),
+                      CustomTextField(
+                        controller: _emailController,
+                        labelText: 'Email',
+                        hintText: 'Ingresa tu email',
+                        prefixIcon: Icons.email,
+                        suffixIcon: Icons.close,
+                        keyboardType: TextInputType.emailAddress,
+                        onSuffixIconTap: () {
+                          _emailController.clear();
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Por favor, ingresa tu email';
+                          }
+                          return null;
+                        },
+                      ),
+                      CustomTextField(
+                        controller: _passwordController,
+                        labelText: 'Contraseña',
+                        hintText: 'Ingresa tu contraseña',
+                        prefixIcon: Icons.lock,
+                        suffixIcon: _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        keyboardType: TextInputType.text,
+                        obscureText: !_isPasswordVisible,
+                        onSuffixIconTap: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Por favor, ingresa tu contraseña';
+                          }
+                          return null;
+                        },
+                      ),
+                      CustomButton(
+                        onPressed: createAccount,
+                        text: 'CREAR CUENTA',
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Center(
+                              child: Container(
+                                height: 1,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              'O',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                          Expanded(
+                            child: Center(
+                              child: Container(
+                                height: 1,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      GoogleSignInButton(
+                        onPressed: createAccountWithGoogle,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/widgets/CustomButton.dart
+++ lib/views/widgets/CustomButton.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
+class CustomButton extends StatefulWidget {
   final VoidCallback onPressed;
   final String text;
   final double width; // Ancho del botón
@@ -8,27 +8,46 @@ class CustomButton extends StatelessWidget {
   CustomButton({required this.onPressed, required this.text, this.width = double.infinity});
 
   @override
+  _CustomButtonState createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  bool _isHovered = false; // Estado para saber si el botón está siendo sobrevolado
+
+  @override
   Widget build(BuildContext context) {
-    return SizedBox( // Usamos SizedBox para controlar el ancho
-      width: width, // Ancho del botón
-      child: ElevatedButton(
-        onPressed: onPressed,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            fontWeight: FontWeight.w700, // <-- Texto más grueso
+    return SizedBox(
+      width: widget.width, // Ancho del botón
+      child: MouseRegion(
+        onEnter: (_) {
+          setState(() {
+            _isHovered = true; // Cambia el estado cuando el mouse entra
+          });
+        },
+        onExit: (_) {
+          setState(() {
+            _isHovered = false; // Cambia el estado cuando el mouse sale
+          });
+        },
+        child: ElevatedButton(
+          onPressed: widget.onPressed,
+          child: Text(
+            widget.text,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
           ),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.grey[600], //color
-          padding: EdgeInsets.symmetric(vertical: 16), // Padding vertical
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: _isHovered ? Colors.grey[500] : Colors.grey[600], // Cambio de color al estar sobre el botón
+            padding: EdgeInsets.symmetric(vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 4,
+            shadowColor: Colors.grey.withOpacity(0.3),
           ),
-          elevation: 4, // Elevación del botón
-          shadowColor: Colors.grey.withOpacity(0.3), // Color de la sombra
         ),
       ),
     );
